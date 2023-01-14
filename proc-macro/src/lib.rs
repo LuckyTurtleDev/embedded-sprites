4510fb89c1207dc510f52878a5bce8764d74a309
LuckyTurtleDev <git@lukas1818.de>
2023-01-14 20:20:31 +0100

    start proc-macro

---
+++ proc-macro/src/lib.rs
@@ -0,0 +1,37 @@
+extern crate proc_macro;
+
+
+use image::{io::Reader as ImageReader, Pixel, RgbaImage, ImageOutputFormat};
+use proc_macro::TokenStream;
+
+#[derive(Debug)]
+struct Color {
+	r: u8,
+	g: u8,
+	b: u8,
+	a: bool,
+}
+
+#[proc_macro]
+pub fn include_image(_item: TokenStream) -> TokenStream {
+	let image = ImageReader::open("img/grass.png")
+		.expect("failed to open image")
+		.decode()
+		.expect("failed to decode image");
+	let image: RgbaImage = image.into_rgba8();
+	let pixels = image.pixels();
+	let mut colors = Vec::new();
+	for pixel in pixels {
+		let mut chanel = pixel.channels().iter();
+		let color = Color {
+			r: chanel.next().unwrap_or(&0).to_owned(),
+			g: chanel.next().unwrap_or(&0).to_owned(),
+			b: chanel.next().unwrap_or(&0).to_owned(),
+			a: chanel.next().map(|value| value == &0 ).unwrap_or(false),
+		};
+		colors.push(color)
+	}
+	let output = format!("{colors:#?}");
+	let output = "r#\"".to_owned() + &output + "\"#";
+	output.parse().unwrap()
+}
