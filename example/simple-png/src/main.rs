77892147f195d5795690d46e57c6c0de35a1624c
LuckyTurtleDev <git@lukas1818.de>
2023-01-16 21:42:02 +0100

    more proc macro

--- example/simple-png/src/main.rs
+++ example/simple-png/src/main.rs
@@ -8,24 +8,11 @@ type Color = Bgr565;
 
 const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
-const IMAGE: Image<Color> = unwrap_ctx!(Image::new(
-	&[
-		Color::new(255, 0, 0),
-		Color::new(0, 255, 0),
-		Color::new(0, 0, 255),
-		Color::new(255, 0, 255),
-		Color::new(255, 255, 255),
-		Color::new(255, 255, 255),
-	],
-	&bitarr![const 0,0,0,0,1,0],
-	3,
-	2
-));
+const IMAGE: Image<Color> = unwrap_ctx!(include_image!());
 
 const SPRITE: Sprite<Color> = Sprite::new(Point::new(15, 15), &IMAGE);
 
 fn main() {
-	println!("{}", include_image!());
 	let mut display = SimulatorDisplay::<Bgr565>::new(Size::new(128, 64));
 	Pixel(Point::new(0, 10), Bgr565::new(0, 255, 0)).draw(&mut display).unwrap();
 	SPRITE.draw(&mut display).unwrap();
--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -1,36 +1,43 @@
 extern crate proc_macro;
 
-use image::{io::Reader as ImageReader, ImageOutputFormat, Pixel, RgbaImage};
+use embedded_graphics::pixelcolor::RgbColor;
+use image::{io::Reader as ImageReader, Pixel, RgbaImage};
 use proc_macro::TokenStream;
-
-#[derive(Debug)]
-struct Color {
-	r: u8,
-	g: u8,
-	b: u8,
-	a: bool,
-}
+use embedded_graphics::pixelcolor::*;
 
 #[proc_macro]
 pub fn include_image(_item: TokenStream) -> TokenStream {
+	type TargetColor = embedded_graphics::pixelcolor::Bgr565;
 	let image = ImageReader::open("img/grass.png")
 		.expect("failed to open image")
 		.decode()
 		.expect("failed to decode image");
+	// convert input image  to vec of colors
 	let image: RgbaImage = image.into_rgba8();
 	let pixels = image.pixels();
-	let mut colors = Vec::new();
+	let mut colors: Vec<TargetColor> = Vec::new();
 	for pixel in pixels {
 		let mut chanel = pixel.channels().iter();
-		let color = Color {
-			r: chanel.next().unwrap_or(&0).to_owned(),
-			g: chanel.next().unwrap_or(&0).to_owned(),
-			b: chanel.next().unwrap_or(&0).to_owned(),
-			a: chanel.next().map(|value| value == &0).unwrap_or(false),
-		};
-		colors.push(color)
+		// `Color::new()` does only cut bits; So create an Bgr888 and convert it to TargetColor
+		let color = Bgr888::new(chanel.next().unwrap_or(&0).to_owned(), chanel.next().unwrap_or(&0).to_owned(), chanel.next().unwrap_or(&0).to_owned());
+		let a = chanel.next().map(|value| value == &0).unwrap_or(false);
+		colors.push(color.into());
 	}
-	let output = format!("{colors:#?}");
-	let output = "r#\"".to_owned() + &output + "\"#";
+	// contruct output
+	let tmap_array = "bitarr![const 0,0,]";
+	let mut color_array = "[ ".to_owned();
+	for color in colors.iter() {
+		color_array += &format!("Color::new({}, {}, {}),", color.r(), color.g(), color.b());
+	}
+	color_array += "]";
+	println!("{tmap_array}");
+	let output = format!(
+		"Image::new(&{},&{},{},{})",
+		color_array,
+		tmap_array,
+		image.width(),
+		image.height()
+	);
+	println!("{output}");
 	output.parse().unwrap()
 }
