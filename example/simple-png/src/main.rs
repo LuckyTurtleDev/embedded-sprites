52cda6fbd9be63748d100a6d8037b5a6e0a9b510
msrd0 <msrd0@users.noreply.github.com>
2023-01-17 14:53:57 +0100

    Improve macro (#2)

--- example/simple-png/src/main.rs
+++ example/simple-png/src/main.rs
@@ -2,15 +2,13 @@ use bitvec::{bitarr, prelude::*};
 use embedded_graphics::{geometry::Point, pixelcolor::Bgr565, prelude::Size, Drawable, Pixel};
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
 use embedded_sprites::{image::Image, include_image, sprite::Sprite};
-use konst::result::unwrap_ctx;
-
-type Color = Bgr565;
 
 const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
-const IMAGE: Image<Color> = unwrap_ctx!(include_image!());
+#[include_image]
+const IMAGE: Image<Bgr565> = "img/grass.png";
 
-const SPRITE: Sprite<Color> = Sprite::new(Point::new(15, 15), &IMAGE);
+const SPRITE: Sprite<Bgr565> = Sprite::new(Point::new(15, 15), &IMAGE);
 
 fn main() {
 	let mut display = SimulatorDisplay::<Bgr565>::new(Size::new(128, 64));
--- proc-macro/src/lib.rs
+++ proc-macro/src/lib.rs
@@ -1,46 +1,84 @@
-extern crate proc_macro;
-
-use embedded_graphics::pixelcolor::{RgbColor, *};
+use embedded_graphics::pixelcolor::{Bgr888, RgbColor as _};
 use image::{io::Reader as ImageReader, Pixel, RgbaImage};
 use proc_macro::TokenStream;
+use proc_macro2::TokenStream as TokenStream2;
+use quote::quote;
+use syn::{parse_macro_input, spanned::Spanned as _, Expr, ExprLit, ItemConst, Lit};
 
-#[proc_macro]
-pub fn include_image(_item: TokenStream) -> TokenStream {
-	type TargetColor = embedded_graphics::pixelcolor::Bgr565;
-	let image = ImageReader::open("img/grass.png")
-		.expect("failed to open image")
+fn expand(
+	ItemConst {
+		attrs,
+		vis,
+		const_token,
+		ident,
+		colon_token,
+		ty,
+		eq_token,
+		expr,
+		semi_token,
+	}: ItemConst,
+) -> syn::Result<TokenStream2> {
+	let path = match expr.as_ref() {
+		Expr::Lit(ExprLit { lit: Lit::Str(path), .. }) => path,
+		expr => return Err(syn::Error::new(expr.span(), "Expected path to image")),
+	};
+	let image = ImageReader::open(path.value())
+		.map_err(|err| syn::Error::new(path.span(), format!("Failed to open image: {err}")))?
 		.decode()
-		.expect("failed to decode image");
-	// convert input image  to vec of colors
+		.map_err(|err| syn::Error::new(path.span(), format!("Failed to decode image: {err}")))?;
+
+	// convert input image to vec of colors
 	let image: RgbaImage = image.into_rgba8();
 	let pixels = image.pixels();
-	let mut colors: Vec<TargetColor> = Vec::new();
+	let mut colors: Vec<Bgr888> = Vec::new();
 	for pixel in pixels {
 		let mut chanel = pixel.channels().iter();
-		// `Color::new()` does only cut bits; So create an Bgr888 and convert it to TargetColor
+		// `Color::new()` only cuts bits; so we create a Bgr888 and convert it to
+		// our target color type
 		let color = Bgr888::new(
 			chanel.next().unwrap_or(&0).to_owned(),
 			chanel.next().unwrap_or(&0).to_owned(),
 			chanel.next().unwrap_or(&0).to_owned(),
 		);
 		let a = chanel.next().map(|value| value == &0).unwrap_or(false);
-		colors.push(color.into());
+		colors.push(color);
 	}
+
 	// contruct output
-	let tmap_array = "bitarr![const 0,0,]";
-	let mut color_array = "[ ".to_owned();
-	for color in colors.iter() {
-		color_array += &format!("Color::new({}, {}, {}),", color.r(), color.g(), color.b());
-	}
-	color_array += "]";
-	println!("{tmap_array}");
-	let output = format!(
-		"Image::new(&{},&{},{},{})",
-		color_array,
-		tmap_array,
-		image.width(),
-		image.height()
-	);
-	println!("{output}");
-	output.parse().unwrap()
+	let tmap_array = quote!(bitarr![const 0,0,]);
+	let color_ty = quote!(<#ty as ::embedded_sprites::private::Image>::Color);
+	let colors = colors.into_iter().map(|color| {
+		let r = color.r();
+		let g = color.g();
+		let b = color.b();
+		quote!({
+			let (r, g, b) = ::embedded_sprites::private::convert_from_bgr888::
+				<#color_ty>(#r, #g, #b);
+			#color_ty::new(r, g, b)
+		})
+	});
+	let color_array = quote!([#(#colors),*]);
+	let width = image.width() as u16;
+	let height = image.height() as u16;
+	let output = quote! {
+		#(#attrs)* #vis #const_token #ident #colon_token #ty #eq_token {
+			const COLOR_ARRAY: &[#color_ty] = &#color_array;
+			match ::embedded_sprites::image::Image::<'static, #color_ty>::new(
+				&COLOR_ARRAY, &#tmap_array, #width, #height
+			) {
+				::core::result::Result::Ok(img) => img,
+				_ => panic!("Failed to construct image")
+			}
+		}
+		#semi_token
+	};
+	//eprintln!("{output}");
+	Ok(output)
+}
+
+#[proc_macro_attribute]
+pub fn include_image(_attr: TokenStream, item: TokenStream) -> TokenStream {
+	expand(parse_macro_input!(item))
+		.unwrap_or_else(|err| err.into_compile_error())
+		.into()
 }
