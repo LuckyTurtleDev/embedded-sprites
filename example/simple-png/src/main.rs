c6c1666d937cdc1f5bd376df7503971fbb7dae91
luckyturtledev <git@lukas1818.de>
2023-05-23 18:56:19 +0200

    add example for bug

--- example/simple-png/src/main.rs
+++ example/simple-png/src/main.rs
@@ -1,18 +1,27 @@
-use embedded_graphics::{geometry::Point, pixelcolor::Bgr565, prelude::Size, Drawable, Pixel};
+use embedded_graphics::{
+	geometry::Point,
+	pixelcolor::Bgr565,
+	prelude::{DrawTarget, RgbColor, Size},
+	Drawable,
+};
 use embedded_graphics_simulator::{OutputSettingsBuilder, SimulatorDisplay, Window};
 use embedded_sprites::{image::Image, include_image, sprite::Sprite};
 
 const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[include_image]
-const IMAGE: Image<Bgr565> = "img/grass.png";
+const IMG_GRASS: Image<Bgr565> = "img/grass.png";
+#[include_image]
+const IMG_FRAME: Image<Bgr565> = "img/frame.png";
 
-const SPRITE: Sprite<Bgr565> = Sprite::new(Point::new(15, 15), &IMAGE);
+const SPRITE_GRASS: Sprite<Bgr565> = Sprite::new(Point::new(15, 15), &IMG_GRASS);
+const SPRITE_FRAME: Sprite<Bgr565> = Sprite::new(Point::new(70, 3), &IMG_FRAME);
 
 fn main() {
 	let mut display = SimulatorDisplay::<Bgr565>::new(Size::new(128, 64));
-	Pixel(Point::new(0, 10), Bgr565::new(0, 255, 0)).draw(&mut display).unwrap();
-	SPRITE.draw(&mut display).unwrap();
+	display.clear(Bgr565::RED).unwrap();
+	SPRITE_GRASS.draw(&mut display).unwrap();
+	SPRITE_FRAME.draw(&mut display).unwrap();
 	let output_settings = OutputSettingsBuilder::new().scale(10).build();
 	Window::new(CARGO_PKG_NAME, &output_settings).show_static(&display);
 }
